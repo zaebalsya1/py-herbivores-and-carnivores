75d270336288006aee6937885c6f42719902ed35
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-01-24 14:57:14 +0200

    Init

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-# Python boilerplate for GitHub tasks
+# Herbivores and carnivores
 
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
-- Implement the task described [here](app/main.py)
+
+
+Nature is well suited to reflect the principles of Object Oriented Programming. 
+All instances of the `Animal` class must have health, name and hidden attribute. 
+Health is 100 and hidden is false by default. Animal should have method 
+`update_animal`, this method updates information about animal in class 
+attribute `Animal.alive`.
+
+All alive animals should be in the class attribute `Animal.alive`. 
+It is a list with animals dicts with keys 'name', 'health', 'hidden'.
+If the health of the animal reaches 0, the beast dies and it should 
+be removed from `Animals.alive`.
+
+Create a `Herbivore` class. This class should inherit from Animal. 
+Herbivore has a method of `hide`, which changes 
+the hidden property of the beast to the opposite value and helps to hide 
+from carnivores. To update information about changed animal call his method 
+`update_animal`. As you remember child classes inherits methods from parents.
+```python
+rabbit = Herbivore("Susan")
+Animal.alive == [{"name": "Susan", "health": 100, "hidden": False}]
+rabbit.hide()
+Animal.alive == [{"name": "Susan", "health": 100, "hidden": True}]  #updated
+```
+
+Create a `Сarnivore` class. This class should inherit from Animal. 
+Carnivore has a `bite` method, which takes a 
+herbivore object and decreases the object's health by 50. The method 
+does not work if it is another сarnivore, or the herbivore is currently hiding.
+To update information about changed animal call his method `update_animal`.
+```python
+lion = Carnivore("Lion King")
+rabbit = Herbivore("Susan")
+rabbit.health == 100
+lion.bite(rabbit)
+rabbit.health == 50  # bited
+
+rabbit.hide()
+lion.bite(rabbit)
+rabbit.health == 50  # lion cannot bite hidden rabbit
+
+rabbit.hide()
+lion.bite(rabbit)
+rabbit.health == 0  # rabbit is dead
+
+Animal.alive == [{"name": "Lion King", "health": 100, "hidden": False}]
+# there is no dead animals in Animal.alive
+```
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,113 @@
-# TODO: add tests
-from app.main import hello_world
+from app.main import Animal, Herbivore, Carnivore
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+def test_animal_class():
+    assert hasattr(Animal, "alive"), (
+        f"Animal class should have attribute 'alive'"
+    )
+    assert hasattr(Animal, "update_animal"), (
+        f"Animal class should have attribute 'update_animal'"
+    )
+
+
+def test_animal_constructor():
+    lion = Animal("Lion King")
+    assert hasattr(lion, "name"), (
+        "Animal instance should have attribute 'name'"
+    )
+    assert hasattr(lion, "health"), (
+        "Animal instance should have attribute 'health'"
+    )
+    assert hasattr(lion, "hidden"), (
+        "Animal instance should have attribute 'hidden'"
+    )
+    assert lion.name == "Lion King", (
+        f"'lion.name' should equal to 'Lion King' when "
+        f"'lion' created by 'Animal('Lion King')'"
+    )
+    assert lion.health == 100, (
+        f"'lion.health' should equal to 100 when "
+        f"'lion' created by 'Animal('Lion King')'"
+    )
+    assert lion.hidden is False, (
+        f"'lion.hidden' should equal to False when "
+        f"'lion' created by 'Animal('Lion King')'"
+    )
+    assert Animal.alive == [{"name": "Lion King", "health": 100, "hidden": False}], (
+        "Constructor should add created animal to 'Animal.alive'"
+    )
+
+
+def test_animal_update_animal():
+    Animal.alive = []
+    rabbit = Animal("Susan")
+    rabbit.health = 50
+    rabbit.hidden = True
+    rabbit.update_animal()
+    assert Animal.alive == [{"name": "Susan", "health": 50, "hidden": True}], (
+        "Method 'update_animal' should update information about animal in Animal.alive"
+    )
+
+
+def test_carnivore_is_subclass():
+    assert issubclass(Carnivore, Animal), (
+        f"Carnivore class should be subclass of Animal"
+    )
+
+
+def test_herbivore_is_subclass():
+    assert issubclass(Herbivore, Animal), (
+        f"Herbivore class should be subclass of Animal"
+    )
+
+
+def test_carnivore_bite_not_hidden():
+    Animal.alive = []
+    lion = Carnivore("King Lion")
+    rabbit = Herbivore("Susan")
+    lion.bite(rabbit)
+    assert rabbit.health == 50, (
+        "If initial health of rabbit equals 100 and rabbit is not hidden "
+        "health should equal to 50 after bite."
+    )
+    assert Animal.alive == [
+        {"name": "King Lion", "health": 100, "hidden": False},
+        {"name": "Susan", "health": 50, "hidden": False}
+    ], "Information in Animals.alive should update after each change"
+
+
+def test_carnivore_bite_hidden():
+    Animal.alive = []
+    lion = Carnivore("King Lion")
+    rabbit = Herbivore("Susan")
+    rabbit.hide()
+    lion.bite(rabbit)
+    assert rabbit.health == 100, (
+        "Carnivore cannot bite hidden herbivore"
+    )
+    assert Animal.alive == [
+        {"name": "King Lion", "health": 100, "hidden": False},
+        {"name": "Susan", "health": 100, "hidden": True}
+    ], "Carnivore cannot bite hidden animal"
+
+
+def test_carnivore_bite_to_death():
+    Animal.alive = []
+    lion = Carnivore("King Lion")
+    pantera = Carnivore("Bagira")
+    rabbit = Herbivore("Susan")
+    lion.bite(rabbit)
+    pantera.bite(rabbit)
+    assert Animal.alive == [
+        {"name": "King Lion", "health": 100, "hidden": False},
+        {"name": "Bagira", "health": 100, "hidden": False}
+    ], "There is no dead animal in Animals.alive"
+
+
+def test_herbivore_hide():
+    Animal.alive = []
+    rabbit = Herbivore("Susan")
+    rabbit.hide()
+    assert Animal.alive == [{"name": "Susan", "health": 100, "hidden": True}], (
+        "Information in Animals.alive should update after each change"
+    )

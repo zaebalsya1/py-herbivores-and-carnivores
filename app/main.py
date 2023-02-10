5e9b69f65e0dab4c0831d55f1b8cf970f6d97a4f
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-02-10 14:53:34 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ class Animal:
     alive = []
 
     def __init__(
-            self, name: str,
+            self,
+            name: str,
             health: int = 100,
             hidden: bool = False
     ) -> None:
@@ -12,10 +13,9 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        name = self.name
-        health = self.health
-        hidden = self.hidden
-        return f"{{Name: {name}, Health: {health}, Hidden: {hidden}}}"
+        return f"{{Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
@@ -24,7 +24,8 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore) -> None:
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
         if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
             herbivore.health -= 50
         if herbivore.health <= 0:

78c94fe9f599f29300e32f4d662728f69945ff7c
zaeba <zaebalsya13@gmail.com>
2024-11-27 14:43:25 +0200

    .!.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive: list["Animal"] = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def die(self) -> None:
+        Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: "Animal") -> None:
+        if not isinstance(target, Herbivore):
+            return
+        if target.hidden:
+            return
+
+        target.health -= 50
+        if target.health <= 0:
+            target.die()
